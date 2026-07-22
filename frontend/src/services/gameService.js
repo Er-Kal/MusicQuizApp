22279c4307d9f756b670e4972f07057956e8e329
Er-Kal <75757517+Er-Kal@users.noreply.github.com>
2026-07-22 02:28:24 +0100

    Fixed service connection

--- frontend/src/services/gameService.js
+++ frontend/src/services/gameService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3031/api/game';
+const API_BASE_URL = import.meta.env.VITE_API_URL + "/api/game"; //'http://localhost:3031/api/game';
 
 export const submitPlaylistApi = async (playlistInput) => {
     try{
